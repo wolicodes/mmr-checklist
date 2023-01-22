3355df648fb0b3949afe6f9f9ec471cf5fd9da63
wbu <wbu@witbe.net>
2023-01-22 02:05:51 +0100

    revise data for s3; add duplicate checks handling; add style json for more flexibility

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let totalChecks = 0
 
 const updateCheckedCount = (direction) => {
   totalChecked += direction
-  document.getElementById('numberChecked').innerHTML = totalChecked
-  document.getElementById('percentComplete').innerHTML = Math.floor(totalChecked * 100 / totalChecks)
+  document.getElementById('numberChecked').textContent = totalChecked.toString()
+  document.getElementById('percentComplete').textContent = Math.floor(totalChecked * 100 / totalChecks).toString()
 }
 
 const onResetAll = () => {
@@ -12,15 +12,15 @@ const onResetAll = () => {
     localStorage.clear()
     document.querySelectorAll('input[type=checkbox]').forEach(el => el.checked = false)
     totalChecked = 0
-    document.getElementById('numberChecked').innerHTML = 0
-    document.getElementById('percentComplete').innerHTML = 0
+    document.getElementById('numberChecked').textContent = '0'
+    document.getElementById('percentComplete').textContent = '0'
     Array.from(document.querySelectorAll('.woth'))
       .forEach((el) => el.classList.remove('woth'));
     Array.from(document.querySelectorAll('.woth-on'))
       .forEach((el) => {
         el.classList.remove('woth-on')
         el.classList.add('woth-off')
-        el.innerHTML = '☆'
+        el.textContent = '☆'
       });
   }
 }
@@ -33,11 +33,15 @@ const onCheck = (elem) => {
     localStorage.removeItem(elem.id)
     updateCheckedCount(-1)
   }
+  document.querySelectorAll(`[data-check-name="${elem.dataset.checkName}"]`).forEach(duplicates => {
+    duplicates.checked = !!elem.checked
+  });
 }
 
-const createCheckbox = (id) => {
+const createCheckbox = (id, checkName) => {
   const input = document.createElement('input')
   input.setAttribute('type', 'checkbox')
+  input.dataset.checkName = checkName
   input.id = id
   if (localStorage.getItem(id)) {
     updateCheckedCount(1)
@@ -47,11 +51,11 @@ const createCheckbox = (id) => {
   return input
 }
 
-const createLabel = (id, value) => {
+const createLabel = (id, checkName) => {
   const label = document.createElement('label')
   label.setAttribute('for', id)
   label.className = 'check-label'
-  label.innerHTML = value
+  label.textContent = checkName
   return label
 }
 
@@ -65,7 +69,7 @@ const createTitleCheckbox = (areaName, areaChecks) => {
   }
   input.onchange = () => {
     if (input.checked) {
-      localStorage.setItem(areaName, true)
+      localStorage.setItem(areaName, 'true')
     } else {
       localStorage.removeItem(areaName)
     }
@@ -83,22 +87,22 @@ const createTitleCheckbox = (areaName, areaChecks) => {
 
 const createWothButton = (areaName) => {
   const wothButton = document.createElement('button')
-  wothButton.innerHTML = '☆'
+  wothButton.textContent = '☆'
   wothButton.className = 'woth-button woth-off'
   if (localStorage.getItem(areaName + 'WOTH')) {
     wothButton.classList.remove('woth-off')
     wothButton.classList.add('woth-on')
-    wothButton.innerHTML = '★'
+    wothButton.textContent = '★'
   }
   wothButton.onclick = () => {
-    if (wothButton.innerHTML === '☆') {
-      wothButton.innerHTML = '★'
+    if (wothButton.textContent === '☆') {
+      wothButton.textContent = '★'
       wothButton.classList.remove('woth-off')
       wothButton.classList.add('woth-on')
       document.getElementById(areaName).classList.add('woth')
-      localStorage.setItem(areaName + 'WOTH', true)
+      localStorage.setItem(areaName + 'WOTH', 'true')
     } else {
-      wothButton.innerHTML = '☆'
+      wothButton.textContent = '☆'
       wothButton.classList.remove('woth-on')
       wothButton.classList.add('woth-off')
       document.getElementById(areaName).classList.remove('woth')
@@ -108,11 +112,24 @@ const createWothButton = (areaName) => {
   return wothButton
 }
 
-fetch('data/s3.json')
-  .then(response => response.json())
-  .then(fullChecklist => {
-    totalChecks = Object.values(fullChecklist).flat().length
-    document.getElementById('totalChecks').innerHTML = totalChecks
+const createAreaTitle = (areaName, areaChecks, emoji = null) => {
+  const areaTitle = document.createElement('h2')
+  const label = document.createElement('label')
+  label.setAttribute('for', areaName + 'Title')
+  label.textContent = emoji ? `${emoji} ${areaName}` : areaName
+  areaTitle.append(createTitleCheckbox(areaName, areaChecks), label, createWothButton(areaName))
+  return areaTitle
+}
+
+Promise.all([fetch('data/s3.json'), fetch('data/area-styles.json')])
+  .then(response => Promise.all([response[0].json(), response[1].json()]))
+  .then(data => {
+    const fullChecklist = data[0]
+    const styles = data[1]
+    console.log(Object.values(fullChecklist).flat())
+    console.log([... new Set(Object.values(fullChecklist).flat())])
+    totalChecks = [... new Set(Object.values(fullChecklist).flat())].length
+    document.getElementById('totalChecks').textContent = totalChecks.toString()
 
     Object.keys(fullChecklist).forEach((areaName) => {
       const areaChecks = fullChecklist[areaName]
@@ -121,16 +138,13 @@ fetch('data/s3.json')
       areaChecks.forEach(checkName => {
         const id = areaName + ' ' + checkName
         const line = document.createElement('li')
-        const checkbox = createCheckbox(id)
+        const checkbox = createCheckbox(id, checkName)
         const label = createLabel(id, checkName)
         line.append(checkbox, label)
         list.append(line)
       })
 
-      const areaTitle = document.createElement('h2')
-      areaTitle.innerHTML = `<label for="${areaName}Title">${areaName}</label>`
-      areaTitle.prepend(createTitleCheckbox(areaName, areaChecks))
-      areaTitle.append(createWothButton(areaName))
+      const areaTitle = createAreaTitle(areaName, areaChecks, styles[areaName]['emoji'])
 
       const card = document.createElement('div')
       card.id = areaName
@@ -142,5 +156,4 @@ fetch('data/s3.json')
 
       document.getElementById('masonry').append(card)
     })
-  })
-
+  }).catch(err => console.error(err))
